81708745d59a050b84588ac0d9c1c983296c30fc
Lucas Ruiz <deth_lucas@hotmail.com>
2025-09-09 12:40:58 -0300

    com vagao e sem vagao

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import flet as ft
 import pandas as pd
 from services.pdd_service import PDDService
-import os
- 
+
 
 def dataframe_para_datatable(df: pd.DataFrame) -> ft.DataTable:
     columns = [ft.DataColumn(ft.Text(col)) for col in df.columns]
@@ -15,45 +14,79 @@ def dataframe_para_datatable(df: pd.DataFrame) -> ft.DataTable:
     return ft.DataTable(columns=columns, rows=rows)
 
 
-
 def main(page: ft.Page):
-    user_profile = os.environ['USERPROFILE']
-    path_estoque =  user_profile + r"\Carmel Capital\TECNOLOGIA - Geral\BUSINESS INTELLIGENCE\BASE PDD\RESIDENCE\estoque.csv"
-    path_movimento = user_profile + r"\Carmel Capital\TECNOLOGIA - Geral\BUSINESS INTELLIGENCE\BASE PDD\RESIDENCE\movimento.csv"
-    # Configuração da página
     page.title = "Calculadora PDD"
     page.window_width = 800
     page.window_height = 600
 
-    pdd = PDDService(path_estoque,path_movimento)
-
-    pdd.iniciar_pdd()
-
-    # Elementos da interface
-    titulo = ft.Text("Calculadora de Provisão para Devedores Duvidosos (PDD)", size=24)
-    input_file = ft.FilePicker()
-    btn_carregar = ft.ElevatedButton("Carregar Planilha", on_click=lambda _: input_file.pick_files())
-    tabela_df_final = dataframe_para_datatable(pdd.df_final)
-    #print(pdd.df_final)
+    # Variáveis para armazenar os paths escolhidos
+    path_estoque = None
     
-    # Layout
+
+    # Componentes de escolha de arquivos
+    picker_estoque = ft.FilePicker()
+    
+
+    page.overlay.extend([picker_estoque])
+
+    # Campos para exibir os caminhos escolhidos
+    txt_estoque = ft.Text("Arquivo de estoque: (não selecionado)")
+    
+
+    def selecionar_estoque(e):
+        nonlocal path_estoque
+        if picker_estoque.result and picker_estoque.result.files:
+            path_estoque = picker_estoque.result.files[0].path
+            txt_estoque.value = f"Arquivo de estoque: {path_estoque}"
+            page.update()
+
+   
+
+
+    picker_estoque.on_result = selecionar_estoque
+
+
+    # Função para processar os arquivos
+    def processar_pdd_com_vagao(e):
+        if path_estoque:
+            pdd = PDDService(path_estoque)
+            pdd.iniciar_pdd()
+            tabela_df_final = dataframe_para_datatable(pdd.df_final)
+            page.add(
+                ft.Text(f"Data do arquivo: {pdd.data_ref}"),
+                tabela_df_final
+            )
+        else:
+            page.add(ft.Text("⚠️ Selecione os dois arquivos antes de processar."))
+
+    def processar_pdd_sem_vagao(e):
+        if path_estoque:
+            pdd = PDDService(path_estoque)
+            pdd.iniciar_pdd(False)
+            tabela_df_final = dataframe_para_datatable(pdd.df_final)
+            page.add(
+                ft.Text(f"Data do arquivo: {pdd.data_ref}"),
+                tabela_df_final
+            )
+        else:
+            page.add(ft.Text("⚠️ Selecione os dois arquivos antes de processar."))
+
+    # Botões para seleção de arquivos e processamento
+    btn_sel_estoque = ft.ElevatedButton("Selecionar Estoque", on_click=lambda _: picker_estoque.pick_files())
+    btn_processar = ft.ElevatedButton("Processar PDD com Vagao", on_click=processar_pdd_com_vagao)
+    btn_processar_sem_vagao = ft.ElevatedButton("Processar PDD Sem Vagao", on_click=processar_pdd_sem_vagao)
+
+    # Layout inicial
     page.add(
-            ft.Column([
-                titulo,
-                ft.Text(f'Data do arquivo: {pdd.data_ref}'),
-                ft.Row([btn_carregar]),
-                tabela_df_final  # Aqui está a exibição do DataFrame
-            ])
-        )
+        ft.Column([
+            ft.Text("Calculadora de Provisão para Devedores Duvidosos (PDD)", size=24),
+            txt_estoque,
+            btn_sel_estoque,
+            btn_processar,
+            btn_processar_sem_vagao
+            
+        ])
+    )
 
-    # Lógica para processar arquivos
-    def processar_arquivo(e):
-        if input_file.result.files:
-            caminho = input_file.result.files[0].path
-            df = pd.read_excel(caminho)
-            # Adicione sua lógica de cálculo aqui
-            page.add(ft.Text(f"Arquivo processado: {caminho}"))
-    
-    input_file.on_result = processar_arquivo
 
 ft.app(target=main)
--- src/services/pdd_service.py
+++ src/services/pdd_service.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
 class PDDService:
-    def __init__(self, path_estoque=None, path_movimento=None):
+    def __init__(self, path_estoque=None):
         self.path_estoque = path_estoque
-        self.path_movimento = path_movimento 
         self.df_estoque = None
-        self.df_movimento = None
+        self.estoque_agrupado = None
         self.df_final = None
         self.data_ref = None 
 
-    def iniciar_pdd(self):
+    def iniciar_pdd(self,com_vagao = True):
         self.df_estoque =  pd.read_csv(self.path_estoque, 
                           encoding='ISO-8859-1',
                           on_bad_lines='skip', 
@@ -17,89 +16,107 @@ class PDDService:
                           decimal=',', 
                           thousands='.',  
                           low_memory=False)
-
-        self.df_movimento = pd.read_csv(self.path_movimento, 
-                                encoding='ISO-8859-1',
-                                on_bad_lines='skip', 
-                                delimiter=';',
-                                decimal=',', 
-                                thousands='.',  
-                                low_memory=False) 
         
         self.data_ref = self.df_estoque["DATA_REFERENCIA"].values[0]
-        self.executar_pdd()
+        if com_vagao : 
+            self.executar_pdd_com_vagao()
+        else:
+            self.executar_pdd_sem_vagao()
 
-    
-    def executar_pdd(self):
-        df_estoque = self.filtrar_colunas_estoque(self.df_estoque)
-        df_movimento = self.filtrar_movimento(self.df_movimento)
-        df_estoque_filtrado = self.atualizar_estoque_por_dados_movimento(df_estoque, df_movimento)
-        estoque_agrupado = self.agrupar_estoque(df_estoque_filtrado)
-        estoque_agrupado = self.acrescentar_faixa_pdd(estoque_agrupado)
-        df_final = self.agrupar_df_final(estoque_agrupado)
-        df_final = self.gerar_percentual_pdd(df_final)
-        df_final = self.ordenar_pdd(df_final)
-        df_final = self.multiplicar_pdd(df_final)
-        self.df_final = df_final  # Resultado final
+    def executar_pdd_com_vagao(self):
+        self.data_ref = self.recuperar_data_referencia(self.df_estoque)
+        self.df_estoque = self.filtrar_colunas_estoque(self.df_estoque)        
+        self.estoque_agrupado = self.agrupar_estoque(self.df_estoque) 
+        self.estoque_agrupado = self.acrescentar_faixa_pdd(self.estoque_agrupado)
+        print(self.estoque_agrupado)
+        self.df_final = self.agrupar_df_final(self.estoque_agrupado)
+        self.df_final = self.gerar_percentual_pdd(self.df_final)
+        self.df_final = self.ordenar_pdd(self.df_final)
+        self.df_final = self.multiplicar_pdd(self.df_final)
+        print(self.df_final)
 
+    def executar_pdd_sem_vagao(self):
+        self.data_ref = self.recuperar_data_referencia(self.df_estoque)
+        self.df_estoque = self.filtrar_colunas_estoque(self.df_estoque)        
+        self.estoque_agrupado = self.acrescentar_faixa_pdd(self.df_estoque)        
+        self.df_final = self.agrupar_df_final(self.estoque_agrupado)
+        self.df_final = self.gerar_percentual_pdd(self.df_final)
+        self.df_final = self.ordenar_pdd(self.df_final)
+        self.df_final = self.multiplicar_pdd(self.df_final)
 
     def categorizar_prazo_atual(self,prazo):
-        if prazo <= 0 and prazo >= -5:
-            return "0~5"
-        elif prazo < -5 and prazo >= -30:
-            return "6~30"
+        if prazo <= 0 and prazo >= -30:
+            return "0~30"
         elif prazo < -30 and prazo >= -60:
             return "31~60"
         elif prazo < -60 and prazo >= -90:
             return "61~90"
         elif prazo < -90 and prazo >= -120:
             return "91~120"
-        elif prazo < -120:
-            return "+120"
+        elif prazo < -120 and prazo >= -150:
+            return "121~150"
+        elif prazo < -160 and prazo >= -180:
+            return "151~180"
+        elif prazo < -180 and prazo >= -210:
+            return "181~210"
+        elif prazo < -210 and prazo >= -240:
+            return "211~240"
+        elif prazo < -240 and prazo >= -270:
+            return "241~270"
+        elif prazo < -270 and prazo >= -300:
+            return "271~300"
+        elif prazo < -300 and prazo >= -330:
+            return "301~330"
+        elif prazo < -330 and prazo >= -360:
+            return "331~360"
+        elif prazo < -360:
+            return "+360"
         else:
             return "A vencer"
 
     def percentual_faixa_pdd(self,faixa):
-        if faixa == "0~5":
+        if faixa == "0~30":
             return 0.0      # 0%
-        elif faixa == "6~30":
-            return 0.05      # 5%
         elif faixa == "31~60":
-            return 0.1413    # 14.13%
-        elif faixa == "61~90":
-            return 0.3375    # 33.75%
-        elif faixa == "91~120":
-            return 0.3375    # 33.75%
-        elif faixa == "+120":
-            return 0.7352    
+            return 0.19    # 14.13%
+        elif faixa == "61~90": 
+            return 0.21 
+        elif faixa == "91~120": 
+            return 0.3375  
+        elif faixa == "121~150":
+            return 0.22    
+        elif faixa == "151~180":
+            return 0.24   
+        elif faixa == "181~210":
+            return 0.26   
+        elif faixa == "211~240":
+            return 0.31   
+        elif faixa == "241~270":
+            return 0.38   
+        elif faixa == "271~300":
+            return 0.50
+        elif faixa == "301~330":
+            return 0.78
+        elif faixa == "331~360":
+            return 0.78
+        elif faixa == "+360":
+            return 1
         else:
-            return 0.0     
+            return 0.0       
 
+    def recuperar_data_referencia(self,df):
+        return df["DATA_REFERENCIA"].values[0]
 
-
-    def filtrar_colunas_estoque(self,df_estoque):
-        return df_estoque[["DOC_SACADO","SEU_NUMERO","NU_DOCUMENTO","PRAZO_ATUAL","VALOR_AQUISICAO","VALOR_NOMINAL","VALOR_PRESENTE"]]
-
-    def filtrar_movimento(self,df_movimento):
-        try:
-            if all(col in df_movimento.columns for col in ["SEU_NUMERO", "NUMERO_DOCUMENTO"]):
-                return df_movimento[["SEU_NUMERO", "NUMERO_DOCUMENTO"]]
-            else:
-                print("Colunas 'SEU_NUMERO' ou 'NUMERO_DOCUMENTO' não encontradas. Retornando DataFrame vazio.")
-                return pd.DataFrame(columns=["SEU_NUMERO", "NUMERO_DOCUMENTO"])
-        except Exception as e:
-            print(f"Erro ao filtrar movimento: {str(e)}")
-            return pd.DataFrame(columns=["SEU_NUMERO", "NUMERO_DOCUMENTO"])
-            
-    def atualizar_estoque_por_dados_movimento(self,df_estoque,df_movimento):
-        return  df_estoque[~df_estoque['NU_DOCUMENTO'].isin(df_movimento['NUMERO_DOCUMENTO'])]
+    def filtrar_colunas_estoque(self,df):
+        return df[["DOC_SACADO","SEU_NUMERO","FAIXA_PDD","NU_DOCUMENTO","PRAZO_ATUAL","VALOR_AQUISICAO","VALOR_NOMINAL","VALOR_PRESENTE","DATA_REFERENCIA"]]
 
     def agrupar_estoque(self,df):
         estoque_agrupado = df.groupby('DOC_SACADO').agg({
         'VALOR_AQUISICAO': 'sum',
         'VALOR_NOMINAL': 'sum',
         'VALOR_PRESENTE': 'sum',
-        'PRAZO_ATUAL': 'min'
+        'PRAZO_ATUAL': 'min',
+        'DATA_REFERENCIA': 'first',
         }).reset_index()
         return estoque_agrupado
 
@@ -124,7 +141,7 @@ class PDDService:
 
 
     def ordenar_pdd(self,df):
-        ordem_faixapdd = ["A vencer", "0~5", "6~30", "31~60", "61~90", "91~120","+120"]
+        ordem_faixapdd = ["A vencer", "0~30", "31~60", "61~90","91~120","121~150","151~180","181~210","211~240","241~270","271~300","301~330","331~360","+360"]
         df["FAIXA_PDD"] = pd.Categorical(
             df["FAIXA_PDD"],
             categories=ordem_faixapdd,
@@ -138,3 +155,22 @@ class PDDService:
         df["PDD POR FAIXA"] = df["VALOR_PRESENTE"] * df["% PDD"]
         return df
         
+    def reordenar_colunas(self,df):
+        df
+        df = df[[col for col in df.columns if col != 'DATA_REFERENCIA'] + ['DATA_REFERENCIA']]
+        return df
+
+
+    def gerar_linha_total(self,df_final):
+        data = df_final[["DATA_REFERENCIA"]].iloc[0,0]        
+        linha_total = {
+            'FAIXA_PDD': 'Total',
+            'VALOR_AQUISICAO': df_final['VALOR_AQUISICAO'].sum(),
+            'VALOR_NOMINAL':df_final['VALOR_NOMINAL'].sum(),
+            'VALOR_PRESENTE': df_final['VALOR_PRESENTE'].sum(),
+            '% PDD': np.nan,
+            'PDD POR FAIXA': df_final['PDD POR FAIXA'].sum(),
+            'DATA_REFERENCIA': data
+        }        
+        df_final = pd.concat([df_final, pd.DataFrame([linha_total])], ignore_index=True)
+        return df_final
